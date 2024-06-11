d6f93f917870b6f7c58b01b598e9697a43af85fa
Maria Eduarda <164123811+marias1lva@users.noreply.github.com>
2024-06-10 23:32:09 -0300

    javaScript basico

---
+++ javaScript/aula1/index.js
@@ -0,0 +1,16 @@
+// variáveis compostas = camel case
+// JavaScript possui tipagem dinâmica: infere os tipos de dados
+var meuNome = "Duda Silva"; // string = aspas duplas
+var idade = 18; // number
+var altura = 1.65; // number
+var estudando = true; // ou false = booleano
+
+console.log(meuNome, typeof meuNome);
+console.log(idade, typeof idade);
+console.log(altura, typeof altura);
+console.log(estudando, typeof estudando);
+
+var semConteudo; // variável não definida = undefined
+
+var curso = "Front-end em React", topico = "JavaScript Básico 1"; // é possível declarar várias variáveis em uma única linha
+console.log(curso, topico);
